a6f20f82dd8ee2ed24211d7356f0a2c668513729
priyankkmt <149180584+priyankkmt@users.noreply.github.com>
2023-12-20 16:29:20 +0530

    Update Hermite.h

--- HermiteAlgorithm/Hermite.h
+++ HermiteAlgorithm/Hermite.h
@@ -4,23 +4,18 @@
 #include "pch.h"
 using namespace std;
 
-class HERMITEALGORITHM_API Hermite
-{
+typedef std::vector<float> floatVector;
+
+class HERMITEALGORITHM_API Hermite{
 public:
-	Hermite(Point inP0, Point inP1, Point inP2, Point inP3);
+	Hermite(void);
+
+public:
+	Hermite(std::vector<Point> points);
 	~Hermite();
-
-	void drawCurve(Point inP0, Point inP1, Point inP2, Point inP3, vector<float>& mVertices, vector<float>& mColors);
-
+	void calculateHermite(floatVector& mVertices, floatVector& mColors);
 private:
-	Point mP0;
-	Point mP1;
-	Point mP2;
-	Point mP3;
-
-	float blend1;
-	float blend2;
-	float blend3;
-	float blend4;
+	Point evaluateHermite(double t);
+private:
+	std::vector<Point> mControlPoints;
 };
-
