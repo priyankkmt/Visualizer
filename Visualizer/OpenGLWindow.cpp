a68831a8f49878221179c8cbe23b4a0602117ee2
priyankkmt <149180584+priyankkmt@users.noreply.github.com>
2023-12-25 14:54:01 +0530

    Update OpenGLWindow.cpp

--- Visualizer/OpenGLWindow.cpp
+++ Visualizer/OpenGLWindow.cpp
@@ -6,7 +6,7 @@
 
 #include <QOpenGLContext>
 #include <QOpenGLPaintDevice>
-#include <QOpenGLShaderProgram>-
+#include <QOpenGLShaderProgram>
 #include <QPainter>
 #include <QDebug>
 
@@ -16,6 +16,12 @@ OpenGLWindow::OpenGLWindow(const QColor& background, QWidget* parent) :
     setParent(parent);
 
     setMinimumSize(1000, 1050);
+
+    const QStringList list = { "vertexShader.glsl" , "fragmentShader.glsl" };
+    mShaderWatcher = new QFileSystemWatcher(list, this);
+
+    connect(mShaderWatcher, &QFileSystemWatcher::fileChanged, this, &OpenGLWindow::shaderWatcher);
+
 }
 
 OpenGLWindow::~OpenGLWindow()
@@ -24,8 +30,7 @@ OpenGLWindow::~OpenGLWindow()
 }
 
 void OpenGLWindow::reset()
-
-{ 
+{
     makeCurrent();
     delete mProgram;
     mProgram = nullptr;
@@ -44,8 +49,14 @@ void OpenGLWindow::paintGL()
     glClear(GL_COLOR_BUFFER_BIT);
     mProgram->bind();
     QMatrix4x4 matrix;
-    matrix.ortho(-32.0f, 32.0f, -32.0f, 32.0f, 0.1f, 100.0f); 
+    matrix.ortho(-32.0f, 32.0f, -32.0f, 32.0f, 0.1f, 100.0f);
     matrix.translate(0, 0, -2);
+    matrix.rotate(rotationAngle);
+
+    mProgram->setUniformValue("r", r);
+    mProgram->setUniformValue("g", g);
+    mProgram->setUniformValue("b", b);
+
     mProgram->setUniformValue(m_matrixUniform, matrix);
 
     floatVector vertices;
@@ -109,23 +120,68 @@ void OpenGLWindow::drawPolygon(Shape& s, floatVector& vertices, floatVector& col
     }
 }
 
+void OpenGLWindow::mouseMoveEvent(QMouseEvent* event) {
+    int dx = event->x() - lastPos.x();
+    int dy = event->y() - lastPos.y();
+
+    if (event->buttons() & Qt::LeftButton) {
+        QQuaternion rotX = QQuaternion::fromAxisAndAngle(0.0f, 1.0f, 0.0f, 0.5f * dx);
+        QQuaternion rotY = QQuaternion::fromAxisAndAngle(1.0f, 0.0f, 0.0f, 0.5f * dy);
+
+        rotationAngle = rotX * rotY * rotationAngle;
+        update();
+    }
+    lastPos = event->pos();
+}
+
+QString OpenGLWindow::readShader(QString filePath) {
+    QFile file(filePath);
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return "Invalid file";
+
+    QTextStream stream(&file);
+    QString line = stream.readLine();
+
+    while (!stream.atEnd()) {
+        line.append(stream.readLine());
+    }
+    file.close();
+    return line;
+}
+
+void OpenGLWindow::shaderWatcher()
+{
+    QString vertexShaderSource = readShader("vertexShader.glsl");
+    QString fragmentShaderSource = readShader("fragmentShader.glsl");
+
+    mProgram->release();
+    mProgram->removeAllShaders();
+    mProgram = new QOpenGLShaderProgram(this);
+    mProgram->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
+    mProgram->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+    mProgram->link();
+}
+
+void OpenGLWindow::selectCurveColor()
+{
+    QColorDialog colorDialog(this);
+    QColor selectedColor = colorDialog.getColor();
+
+    if (selectedColor.isValid()) {
+        // Set the selected color to the curves or store it for later use
+        r = selectedColor.redF();
+        g = selectedColor.greenF();
+        b = selectedColor.blueF();
+        emit shapeUpdate();  // Trigger an update to redraw the curves with the new color
+    }
+}
+
+
 void OpenGLWindow::initializeGL()
 {
-    static const char* vertexShaderSource =
-        "attribute highp vec4 posAttr;\n"
-        "attribute lowp vec4 colAttr;\n"
-        "varying lowp vec4 col;\n"
-        "uniform highp mat4 matrix;\n"
-        "void main() {\n"
-        "   col = colAttr;\n"
-        "   gl_Position = matrix * posAttr;\n"
-        "}\n";
-
-    static const char* fragmentShaderSource =
-        "varying lowp vec4 col;\n"
-        "void main() {\n"
-        "   gl_FragColor = col;\n"
-        "}\n";
+    QString vertexShaderSource = readShader("vertexShader.glsl");
+    QString fragmentShaderSource = readShader("fragmentShader.glsl");
 
     initializeOpenGLFunctions();
 
@@ -134,9 +190,9 @@ void OpenGLWindow::initializeGL()
     mProgram->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
     mProgram->link();
     m_posAttr = mProgram->attributeLocation("posAttr");
-    Q_ASSERT(m_posAttr != -1);
-    m_colAttr = mProgram->attributeLocation("colAttr");
-    Q_ASSERT(m_colAttr != -1);
-    m_matrixUniform = mProgram->uniformLocation("matrix");
-    Q_ASSERT(m_matrixUniform != -1);
+    if (m_posAttr == -1 || m_colAttr == -1 || m_matrixUniform == -1)
+    {
+        qDebug() << "Shader attribute or uniform location error.";
+        // Handle the error appropriately, e.g., return or throw an exception
+    }
 }
