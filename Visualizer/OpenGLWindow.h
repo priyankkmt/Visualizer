4e7202e0b94b0d8ce333a606dafa67f3bf60e74d
priyankkmt <149180584+priyankkmt@users.noreply.github.com>
2023-12-25 14:54:24 +0530

    Update OpenGLWindow.h

--- Visualizer/OpenGLWindow.h
+++ Visualizer/OpenGLWindow.h
@@ -21,54 +21,74 @@ typedef std::vector<std::vector<float>> vectorOfVectorFLoat;
 class OpenGLWindow :public QOpenGLWidget, protected QOpenGLFunctions
 {
     Q_OBJECT
+signals:
+    void shapeUpdate();
 public:
     OpenGLWindow(const QColor& background, QWidget* parent);
     ~OpenGLWindow();
-    
+
     void setVectorOfLines(floatVector& vectorOfLines);
     void setColorOfLines(floatVector& colorOfLines);
     void setPolygon(Shape& clippedPolygon);
     void updateData(const floatVector& vertices, const floatVector& colors);
-    
+    void mouseMoveEvent(QMouseEvent* event);
+    void selectCurveColor();
+
 protected:
     void paintGL() override;
     void initializeGL() override;
 
+private slots:
+    void shaderWatcher();
+
 private:
     void reset();
-    //void setRegion(double xMin, double yMin, double xMax, double yMax);
-    
 
     void drawPolygon(Shape& s, floatVector& vertices, floatVector& colors);
 
 private:
     bool mAnimating = false;
-    QOpenGLContext* mContext = nullptr;
+
+    QOpenGLContext*     mContext = nullptr;
     QOpenGLPaintDevice* mDevice = nullptr;
 
-    QOpenGLShader* mVshader = nullptr;
-    QOpenGLShader* mFshader = nullptr;
+    QOpenGLShader*        mVshader = nullptr;
+    QOpenGLShader*        mFshader = nullptr;
     QOpenGLShaderProgram* mProgram = nullptr;
 
-    //QList<QVector3D> mVertices;
-    //QList<QVector3D> mNormals;
+    
+
     QOpenGLBuffer mVbo;
-    int mVertexAttr;
-    int mNormalAttr;
-    int mMatrixUniform;
-    QColor mBackground;
+    int           mVertexAttr;
+    int           mNormalAttr;
+    int           mMatrixUniform;
+    QColor        mBackground;
+
     QMetaObject::Connection mContextWatchConnection;
 
     GLint m_posAttr = 0;
     GLint m_colAttr = 0;
     GLint m_matrixUniform = 0;
+
     QVector<QVector2D> pixelVertices;
 
     Grid grid;
-    //QVector<QVector2D> squareVertices;
 
     floatVector verticesOfOrignalLine;
     floatVector colorOfOrignalLine;
 
     Shape mClipPolygon;
+
+    QQuaternion rotationAngle;
+    QPoint      lastPos;
+
+    QFileSystemWatcher* mShaderWatcher;
+    
+    GLfloat r{ 1.0f };
+    GLfloat g{ 0.0f };
+    GLfloat b{ 1.0f };
+
+    QColor color;
+private:
+    QString readShader(QString filePath);
 };
